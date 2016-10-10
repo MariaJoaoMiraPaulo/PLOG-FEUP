529530a30d41e186209f44fc204e7bf34c663d38
MariaJoaoMiraPaulo <mariajoaomirapaulo1996@gmail.com>
2016-10-10 10:09:40 +0100

    Continuing with board display

--- src/Blockade.pl
+++ src/Blockade.pl
@@ -1,27 +1,35 @@
-/** board(Board):-
+ board(Board):-
   Board = [[vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio],
             [vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio],
             [vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio],
+            [vazio, vazio, vazio, equipa1, vazio, vazio, vazio, equipa2, vazio, vazio, vazio],
             [vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio],
             [vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio],
             [vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio],
             [vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio],
             [vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio],
             [vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio],
+            [vazio, vazio, vazio, equipa1, vazio, vazio, vazio, equipa2, vazio, vazio, vazio],
             [vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio],
             [vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio],
-            [vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio],
-            [vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio],
-            [vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio]]. **/
+            [vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio]].
 
+%TODO: Make a list for --------
+
+/**
 board(Board):-
   Board = [[vazio, vazio, vazio],
         [vazio, vazio, vazio],
         [vazio, vazio, vazio]].
+        **/
 
+board_display:-
+  nl, write(' ---------------------'),nl,
+  board(T),display_board(T).
 
 display_board([L1|LS]):-
   display_line(L1), nl,
+  write(' ---------------------'),nl,
   display_board(LS).
 
 display_board([]):-
@@ -35,3 +43,5 @@ display_line([E1|ES]):-
 display_line([]).
 
 traduz(vazio, ' ').
+traduz(equipa1, 'O').
+traduz(equipa2, 'X').

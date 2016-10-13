430cd9616fcf47f5f859db86cd311aec3c96b9a4
NunoRamos <nunor07.unfc@gmail.com>
2016-10-13 10:26:14 +0100

    Updating board

--- src/Blockade.pl
+++ src/Blockade.pl
@@ -1,39 +1,45 @@
  board(Board):-
-  Board = [[vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio],
-            [vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio],
-            [vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio],
-            [vazio, vazio, vazio, equipa1, vazio, vazio, vazio, equipa2, vazio, vazio, vazio],
-            [vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio],
-            [vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio],
-            [vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio],
-            [vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio],
-            [vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio],
-            [vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio],
-            [vazio, vazio, vazio, equipa1, vazio, vazio, vazio, equipa2, vazio, vazio, vazio],
-            [vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio],
-            [vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio],
-            [vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio, vazio]].
+  Board = [[empty, noWall, empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty],
+            [noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall],
+            [empty, noWall, empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty],
+            [noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall],
+            [empty, noWall, empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty],
+            [noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall],
+            [empty, noWall, empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty],
+            [noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall],
+            [empty, noWall, empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty],
+            [noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall],
+            [empty, noWall, empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty],
+            [noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall],
+            [empty, noWall, empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty],
+            [noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall],
+            [empty, noWall, empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty],
+            [noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall],
+            [empty, noWall, empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty],
+            [noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall],
+            [empty, noWall, empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty],
+            [noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall],
+            [empty, noWall, empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty],
+            [noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall],
+            [empty, noWall, empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty],
+            [noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall],
+            [empty, noWall, empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty],
+            [noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall],
+            [empty, noWall, empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty, noWall,empty]].
 
 %TODO: Make a list for --------
 
-/**
-board(Board):-
-  Board = [[vazio, vazio, vazio],
-        [vazio, vazio, vazio],
-        [vazio, vazio, vazio]].
-        **/
-
 board_display:-
-  nl, write(' ---------------------'),nl,
+  nl, write(' ----------------------'),nl,
   board(T),display_board(T).
 
 display_board([L1|LS]):-
+  write('|'),
   display_line(L1), nl,
-  write(' ---------------------'),nl,
   display_board(LS).
 
 display_board([]):-
-    nl.
+    write(' ----------------------'),nl.
 
 display_line([E1|ES]):-
   traduz(E1,V),
@@ -42,6 +48,8 @@ display_line([E1|ES]):-
 
 display_line([]).
 
-traduz(vazio, ' ').
-traduz(equipa1, 'O').
-traduz(equipa2, 'X').
+traduz(empty, ' ').
+traduz(player1, 'O').
+traduz(player2, 'X').
+traduz(wall, '+').
+traduz(noWall, '-').

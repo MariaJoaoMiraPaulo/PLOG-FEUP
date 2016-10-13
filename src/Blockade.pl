eacc3762b0f45d91dcb656cad00b4d38c4c8e714
NunoRamos <nunor07.unfc@gmail.com>
2016-10-13 11:31:51 +0100

    Correcting bug when writing the board

--- src/Blockade.pl
+++ src/Blockade.pl
@@ -25,31 +25,44 @@
             [noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall],
             [empty,noVerticalWall, empty,noVerticalWall, empty,noVerticalWall, empty,noVerticalWall, empty,noVerticalWall, empty,noVerticalWall, empty,noVerticalWall, empty,noVerticalWall, empty, noVerticalWall, empty,noVerticalWall, empty],
             [noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall, noWall],
-            [empty,noVerticalWall, empty,noVerticalWall, empty,noVerticalWall, empty,noVerticalWall, empty,noVerticalWall, empty,noVerticalWall, empty,noVerticalWall, empty,noVerticalWall, empty, noVerticalWall, empty,noVerticalWall, empty]]
+            [empty,noVerticalWall, empty,noVerticalWall, empty,noVerticalWall, empty,noVerticalWall, empty,noVerticalWall, empty,noVerticalWall, empty,noVerticalWall, empty,noVerticalWall, empty, noVerticalWall, empty,noVerticalWall, empty]].
 
 %TODO: Make a list for --------
 
 board_display:-
-  nl, write(' ----------------------'),nl,
-  board(T),display_board(T).
+  display_x_coord,
+  nl, write(' -------------------------------------------- '),nl,
+  board(T),
+  display_board(T,1).
 
-display_board([L1|LS]):-
+display_board([L1|LS],N):-
   write('|'),
-  display_line(L1), nl,
-  display_board(LS).
+  display_line(L1,N), nl,
+  N1 is N+1,
+  display_board(LS,N1).
 
-display_board([]):-
-    write(' ----------------------'),nl.
+display_board([], 28):-
+    write(' -------------------------------------------- '),nl.
 
-display_line([E1|ES]):-
+display_line([E1|ES], N):-
   traduz(E1,V),
-  write(V), write('|'),
-  display_line(ES).
+  write(V),
+  display_line(ES, N).
 
-display_line([]).
+display_line([], N):-
+  0 is N mod 2,
+  write('|').
 
-traduz(empty, ' ').
-traduz(player1, 'O').
-traduz(player2, 'X').
-traduz(wall, '+').
-traduz(noWall, '*').
+display_line([], N):-
+  1 is N mod 2,
+  write(' |').
+
+display_x_coord:-
+  write('  1   2   3   4   5   6   7   8   9   10  11 ').
+
+traduz(empty, '   ').
+traduz(player1, ' O ').
+traduz(player2, ' X ').
+traduz(wall, ' *  ').
+traduz(noWall, ' -  ').
+traduz(noVerticalWall, '|').

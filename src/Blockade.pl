bb4db94e44dc2740837e2fa560fc2b52dbc8f567
NunoRamos <nunor07.unfc@gmail.com>
2016-10-13 11:36:00 +0100

    Adding y coords to the display of the board

--- src/Blockade.pl
+++ src/Blockade.pl
@@ -31,18 +31,19 @@
 
 board_display:-
   display_x_coord,
-  nl, write(' -------------------------------------------- '),nl,
+  nl, write('   -------------------------------------------- '),nl,
   board(T),
   display_board(T,1).
 
 display_board([L1|LS],N):-
+  display_y_coord(N),
   write('|'),
   display_line(L1,N), nl,
   N1 is N+1,
   display_board(LS,N1).
 
 display_board([], 28):-
-    write(' -------------------------------------------- '),nl.
+    write('   -------------------------------------------- '),nl.
 
 display_line([E1|ES], N):-
   traduz(E1,V),
@@ -59,6 +60,14 @@ display_line([], N):-
 
 display_x_coord:-
   write('  1   2   3   4   5   6   7   8   9   10  11 ').
+
+display_y_coord(N):-
+  N<10,
+  write(N),
+  write(' ').
+
+display_y_coord(N):-
+  write(N).
 
 traduz(empty, '   ').
 traduz(player1, ' O ').

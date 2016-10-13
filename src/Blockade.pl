8540705fb53a9e6b7a73e48788d1de9d54a2b54d
NunoRamos <nunor07.unfc@gmail.com>
2016-10-13 12:40:06 +0100

    Adding y coord

--- src/Blockade.pl
+++ src/Blockade.pl
@@ -33,16 +33,16 @@ board_display:-
   display_x_coord,
   nl, write('   -------------------------------------------- '),nl,
   board(T),
-  display_board(T,1).
+  display_board(T,1,1).
 
-display_board([L1|LS],N):-
-  display_y_coord(N),
+display_board([L1|LS],N, X1):-
+  display_y_coord(N, X1, X2),
   write('|'),
   display_line(L1,N), nl,
   N1 is N+1,
-  display_board(LS,N1).
+  display_board(LS,N1, X2).
 
-display_board([], 28):-
+display_board([], 28, 15):-
     write('   -------------------------------------------- '),nl.
 
 display_line([E1|ES], N):-
@@ -59,15 +59,24 @@ display_line([], N):-
   write(' |').
 
 display_x_coord:-
-  write('  1   2   3   4   5   6   7   8   9   10  11 ').
+  write('   1   2   3   4   5   6   7   8   9   10  11 ').
 
-display_y_coord(N):-
-  N<10,
-  write(N),
-  write(' ').
+display_y_coord(N, X1, X2):-
+  X1<10,
+  1 is N mod 2,
+  write(X1),
+  write(' '),
+  X2 is X1+1.
 
-display_y_coord(N):-
-  write(N).
+display_y_coord(N, X1, X2):-
+  1 is N mod 2,
+  write(X1),
+  X2 is X1+1.
+
+display_y_coord(N, X1, X2):-
+  0 is N mod 2,
+  X2 is X1,
+  write('  ').
 
 traduz(empty, '   ').
 traduz(player1, ' O ').

9c3e34a11143555882ab958d879e344bd581091e
NunoRamos <nunor07.unfc@gmail.com>
2016-11-10 18:29:28 +0000

    Fixing a bug

---
+++ src/blockade.pl
@@ -0,0 +1,11 @@
+ :- ensure_loaded('displayBoard.pl').
+ :- ensure_loaded('utilitiesBoard.pl').
+ :- ensure_loaded('userInput.pl').
+ :- ensure_loaded('gameLogic.pl').
+
+pos:-
+  emptyBoard(T),
+  returnPosition(startPlayer1WithPawn,T,1,1,Xf,Yf),
+  write(Xf),
+  write(Yf).
+  

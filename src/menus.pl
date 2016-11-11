23d57287e3c9d10fdd16077837323a9c3b430d84
MariaJoaoMiraPaulo <mariajoaomirapaulo1996@gmail.com>
2016-11-11 16:57:07 +0000

    How To play Menu

--- src/menus.pl
+++ src/menus.pl
@@ -7,11 +7,22 @@ readInput(1):-
   normalBoard(T),
   gameLoop(T, 21, 27).
 
+readInput(2):-
+  printHowToPlayMenu,
+  read(InputHowToPlay),
+  readHowToPlayInput(InputHowToPlay).
+
 readInput(3):-
   write('Exiting...').
 
 readInput(Input):-
   write('Error: invalid input.').
+
+readHowToPlayInput(1):-
+    mainMenu.
+
+readHowToPlayInput(InputHowToPlay):-
+    write('Error: invalid input.').
 
 printMainMenu:-
   nl,nl,nl,
@@ -40,3 +51,38 @@ printMainMenu:-
   write('        |                                                                         |'),nl,
   write('        |                           Choose an option...                           |'),nl,
   write('        |-------------------------------------------------------------------------| '),nl,nl,nl,nl.
+
+
+printHowToPlayMenu:-
+  nl,nl,nl,
+  write('        |-------------------------------------------------------------------------|'),nl,
+  write('        |                                                                         |'),nl,
+  write('        |                                                                         |'),nl,
+  write('        |                                                                         |'),nl,
+  write('        |           _   _                 _         ______ _                      |'),nl,
+  write('        |          | | | |               | |        | ___ | |                     |'),nl,
+  write('        |          | |_| | _____      __ | |_ ___   | |_/ / | __ _ _   _          |'),nl,
+  write('        |          |  _  |/ _  |  /| / / | __/ _  |  |  __/| |/ _` | | | |        |'),nl,
+  write('        |          | | | | (_) | V  V /  | || (_) | | |   | | (_| | |_| |         |'),nl,
+  write('        |          |_| |_/|___/ |_/|_/   | __|___/  |_|   |_||__,_||__, |         |'),nl,
+  write('        |                                                           __/ |         |'),nl,
+  write('        |                                                           |___/         |'),nl,
+  write('        |                                                                         |'),nl,
+  write('        |                                                                         |'),nl,
+  write('        |   Players each have 2 pawns, 9 walls (which are placed vertically),and  |'),nl,
+  write('        |   9 walls (placed horizontally). Pawns start on the symbols matching    |'),nl,
+  write('        |   them, which are 4x4 squares in on each of the four corners of the     |'),nl,
+  write('        |   board. The objective is simple: to get one of your pawns to a         |'),nl,
+  write('        |   starting spot of your opponent.                                       |'),nl,
+  write('        |    Each turn players can move one pawn up to two spaces, and also place |'),nl,
+  write('        |   one wall, useful for blocking off their opponent s move.              |'),nl,
+  write('        |    Walls always cover two squares worth of area, and must be place      |'),nl,
+  write('        |    according to their type (vertically or horizontally).                |'),nl,
+  write('        |                                                                         |'),nl,
+  write('        |   Once players are out of walls, they keep moving pawns until one wins. |'),nl,
+  write('        |                                                                         |'),nl,
+  write('        |                                                                         |'),nl,
+  write('        |                                                                         |'),nl,
+  write('        |                                                                         |'),nl,
+  write('        |                             1. Main Menu                                |'),nl,
+  write('        |-------------------------------------------------------------------------|'),nl,nl,nl,nl.

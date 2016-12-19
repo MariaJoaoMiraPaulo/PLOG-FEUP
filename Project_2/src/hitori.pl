00739e3e32b659145d61e4dc8256264151efa162
NunoRamos <nunor07.unfc@gmail.com>
2016-12-19 19:59:19 +0000

    Continuing work, adding display function

--- Project_2/src/hitori.pl
+++ Project_2/src/hitori.pl
@@ -11,7 +11,7 @@ Puzzle = [[4,8,1,6,3,2,5,7],
           [3,5,6,7,3,1,8,4],
           [6,4,2,3,5,4,7,8],
           [8,7,1,4,2,3,5,6]].
-
+/*
 %Dominio vai ser de 1 ao numero de Colunas
 %Inicializar tabuleiro com a soluação
 initializeBoard([_Line|_Board],_NCol,0).
@@ -21,7 +21,20 @@ initializeBoard([Line|Board],NCol,NLin):-
   length(Line,NCol),
   domain(Line,1,NCol),
   NL is NLin-1,
-  initializeBoard(Board,NCol,NL).
+  initializeBoard(Board,NCol,NL).*/
+
+%Dominio vai ser de 1 ao numero de Colunas
+%Inicializar tabuleiro com a soluação
+initializeBoard([],_NCol).
+
+%length(?List,?Length)
+initializeBoard([Line|Board],NCol):-
+  initializeBoard(Board,NCol),
+  MaxValue is 2 * NCol,
+  length(Line,NCol),
+  domain(Line,1,MaxValue).
+  %NL is NLin-1,
+%  labeling([],Line),
 
 
 testDifferentLines([Line|Column],0).
@@ -57,10 +70,54 @@ checkAdjacentPositions([Line|Board]):- validate(Line,Board),checkAdjacentPositio
 
 validate([Elem|Line],Board):-
 */
-
+/*
 hitori(Puzzle, PuzzleSolution):-
   initializeBoard(PuzzleSolution,8,8),
   testDifferentLines(PuzzleSolution),
   transpose(PuzzleSolution,InvertedPuzzleSolution),
   testDifferentLines(InvertedPuzzleSolution).
-  %restrições.
+  %restrições.*/
+hitori(Puzzle, PuzzleSolution):-
+  /*length(Puzzle,Size),
+  length(PuzzleSolution,Size),
+  transpose(Puzzle,InitialColumns),
+  length(InitialColumns,ColumnsSize),
+%  initializeBoard(PuzzleSolution,ColumnsSize),
+  %domain(SolutionColumns,1,ColumnsSize),*/
+  solver(Puzzle, PuzzleSolution),
+  display_board(PuzzleSolution,9).
+
+
+solver(Puzzle, PuzzleSolution):-
+  length(Puzzle,Size),
+  length(PuzzleSolution,Size),
+  initializeBoard(PuzzleSolution,Size),
+  transpose(PuzzleSolution,TransposePuzzleSolution),
+  % não permitir elementos diferentes
+
+  maplist(all_distinct,PuzzleSolution),
+  maplist(all_distinct,TransposePuzzleSolution),
+
+  maplist(labeling([ff]),PuzzleSolution).
+
+
+
+  display_board([L1|LS], MaxValue):-
+   write('|'),
+   display_line(L1,MaxValue), nl,
+   display_board(LS,MaxValue).
+
+  display_board([],MaxValue).
+
+  display_line([E1|ES],MaxValue):-
+   E1 < MaxValue,
+   write(E1),
+   write('|'),
+   display_line(ES,MaxValue).
+
+  display_line([E1|ES],MaxValue):-
+    write(' '),
+    write('|'),
+    display_line(ES,MaxValue).
+
+  display_line([],MaxValue).
